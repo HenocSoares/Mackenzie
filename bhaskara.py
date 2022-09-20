9683191ca7edb3ed89155815130ceac5cea436f5
Henoc Soares <103623720+soaresnoc@users.noreply.github.com>
2022-09-20 04:05:47 -0300

    Update bhaskara.py

--- bhaskara.py
+++ bhaskara.py
@@ -1,13 +1,13 @@
-import math
+import math,sys
 
 a = int(input("Informe o valor de a: "))
+if a == 0:
+  print("Com o coeficiente angular de valor zero, não há uma equação do segundo grau para calcular!")
+  sys.exit()
 b = int(input("Informe o valor de b: "))
 c = int(input("Informe o valor de c: "))
 
 delta = (b**2)-4*(a*c)
-x  = 0
-x1 = 0
-x2 = 0
 
 if delta == 0:
   x = (-b+(math.sqrt(delta)))/2*a
