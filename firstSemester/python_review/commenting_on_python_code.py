49d2d7d1f8817b122bb241a773c39e5cefdcaa73
Henoc Soares Freire <henocsoares@hotmail.com.br>
2023-12-28 23:58:00 -0300

    Create commenting_on_python_code.py
    Henoc Soares, let's go myself!😆

---
+++ firstSemester/python_review/commenting_on_python_code.py
@@ -0,0 +1,16 @@
+# Please, refer to: https://youtu.be/Gfb9chRRSTI
+# One line comment
+
+"""
+This is a 
+multiline comment
+"""
+
+text = """This is 
+the start of 
+our journey"""
+
+text_II = "This is the\nsecond multiline\ncomment."
+
+print(text)
+print(text_II)
