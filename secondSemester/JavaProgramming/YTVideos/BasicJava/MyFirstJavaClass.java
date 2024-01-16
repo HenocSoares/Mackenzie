cc3d8b58f86f510fc1446de4e9a8de5b37ece875
Henoc Freire <henocsoares@hotmail.com.br>
2024-01-16 15:37:12 -0300

    Create MyFirstJavaClass.java

---
+++ secondSemester/JavaProgramming/YTVideos/BasicJava/MyFirstJavaClass.java
@@ -0,0 +1,16 @@
+// #001 Java Tutorials: Basic Structure
+public class MyFirstJavaClass {
+    public static void main(String[] args) {
+        // Let's print to the screen
+        System.out.println("Welcome to the Java tutorials!");
+
+        // Variables declaration
+        int num1 = 5;
+        int num2 = 28;
+
+        int sum = num1 + num2;
+
+        // Printing the variables
+        System.out.println("The sum of " + num1 + " + " + num2 + " is equal to: " + sum + ".");
+    }
+}
