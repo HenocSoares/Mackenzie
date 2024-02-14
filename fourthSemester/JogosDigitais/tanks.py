d692988131fa3db9f22492035de70191729d37ea
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-14 06:20:29 -0300

    Update tanks.py

--- fourthSemester/JogosDigitais/tanks.py
+++ fourthSemester/JogosDigitais/tanks.py
@@ -1,3 +1,5 @@
+import random
+
 class Tank(object):
     def __init__(self, name):
         self.name = name
@@ -11,7 +13,7 @@ class Tank(object):
         else:
             return "%s (DEAD)" % self.name
 
-    def fire_at(self, enemy):
+    def fires_at(self, enemy):
         if self.ammo >= 1:
             self.ammo -= 1
             print(self.name, "fires on", enemy.name)
@@ -37,3 +39,28 @@ for i in range(5):
 
 for battle_tank in battle_tanks:
     print(battle_tank)
+
+# Randomly shuffle list of battle tanks
+random.shuffle(battle_tanks)
+
+# The tank at index 0 will first attack
+attacking_tank = battle_tanks[0]
+print("Attacking tank:", attacking_tank)
+
+# Shuffling remaining items starting on index 1
+remaining_tanks = battle_tanks[1:]
+random.shuffle(remaining_tanks)
+
+# Tank at index 0 receives the hit
+hit_tank = remaining_tanks[0]
+print("Receiving tank:", hit_tank)
+
+# Attack and hit happens
+attacking_tank.fires_at(hit_tank)
+print(attacking_tank)
+print(hit_tank)
+
+
+# Should it blow?
+if hit_tank.armor <= 0:
+	hit_tank.explode()
