6f415a98aab955e7ccde08931771bb94eee2a977
Henoc Freire <henocsoares@hotmail.com.br>
2024-02-14 06:48:03 -0300

    Update tanks.py
    Code beautified!

--- fourthSemester/JogosDigitais/tanks.py
+++ fourthSemester/JogosDigitais/tanks.py
@@ -5,7 +5,7 @@ class Tank(object):
         self.name = name
         self.alive = True
         self.ammo = 5
-        self.armor = 60 
+        self.armor = 60
 
     def __str__(self):
         if self.alive:
@@ -20,7 +20,7 @@ class Tank(object):
             enemy.hit()
         else:
             print(self.name, "has no shells!")
-    
+
     def hit(self):
         self.armor -= 20
         print(self.name, "is hit")
@@ -33,45 +33,45 @@ class Tank(object):
 
 battle_tanks = []
 for i in range(5):
-    battle_tank = "battleVessel" + str(i+1)
+    battle_tank = "battleVessel" + str(i + 1)
     new_battle_tank = Tank(battle_tank)
     battle_tanks.append(new_battle_tank)
 
 for battle_tank in battle_tanks:
     print(battle_tank)
 
-# Start and repeat the draw process until we have a winner
+# Start and repeat (simulation) the draw process until we have a winner
 while len(battle_tanks) > 1:
-	# Randomly shuffle the list of battle tanks
-	random.shuffle(battle_tanks)
+    # Randomly shuffle the list of battle tanks
+    random.shuffle(battle_tanks)
 
-	# Tank at index [0] will strike first
-	attacking_tank = battle_tanks[0]
-	print("Attacking tank:", attacking_tank)
+    # Tank at index [0] will strike first
+    attacking_tank = battle_tanks[0]
+    print("Attacking tank:", attacking_tank)
 
-	# Shuffling remaining items for first hit to happen from [1]
-	remaining_tanks = battle_tanks[1:]
-	random.shuffle(remaining_tanks)
+    # Shuffling remaining items for first hit to happen from [1]
+    remaining_tanks = battle_tanks[1:]
+    random.shuffle(remaining_tanks)
 
-	# Tank at [0] receives the hit
-	hit_tank = remaining_tanks[0]
-	print("Attacked tank:", hit_tank)
+    # Tank at [0] receives the hit
+    hit_tank = remaining_tanks[0]
+    print("Attacked tank:", hit_tank)
 
-	# Attack plus hit happens
-	attacking_tank.fires_at(hit_tank)
-	print(attacking_tank)
-	print(hit_tank)
+    # Attack plus hit happens
+    attacking_tank.fires_at(hit_tank)
+    print(attacking_tank)
+    print(hit_tank)
 
-	# Check if hit_tank should explode
-	if hit_tank.armor <= 0:
-		hit_tank.explode()
-		# Removing exploded battle vessel via pop()
-		battle_tanks.pop(battle_tanks.index(hit_tank))
+    # Check if hit_tank should explode
+    if hit_tank.armor <= 0:
+        hit_tank.explode()
+        # Removing exploded battle vessel via pop()
+        battle_tanks.pop(battle_tanks.index(hit_tank))
 
-	# Displays the updated state of the tanks after each round
-	print("\nCurrent state:\n")
-	for battle_tank in battle_tanks:
-		print(battle_tank)
+    # Displays the updated state of the tanks after each round
+    print("\nCurrent state:\n")
+    for battle_tank in battle_tanks:
+        print(battle_tank)
 
 # Prints the winner vessel's name
 print("The winner is:", battle_tanks[0])
