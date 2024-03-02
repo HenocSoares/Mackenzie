e1264a5185b04ffe98c032861be41afc74b35199
Henoc Freire <henocsoares@hotmail.com.br>
2024-03-01 23:36:57 -0300

    Create shows_invitation.py

---
+++ firstSemester/beecrowd/shows_invitation.py
@@ -0,0 +1,14 @@
+'''
+Faça um programa em Python que recebe o custo (valor em reais)
+de um espetáculo teatral e o preço do convite (valor em reais)
+desse espetáculo. Esse programa deve calcular e mostrar:
+
+a) A quantidade de convites que devem ser vendidos para que,
+pelo menos, o custo do espetáculo seja realizado.
+b) A quantidade de convites que deverão ser vendidos para que
+você tenha um lucro de 23%.
+
+Observe que as peças calculadas devem ser um número inteiro, 
+portanto, o resultado da quantidade de convites deve ser 
+arredondado para cima, usando a função matemática detalhada em Python.
+'''
