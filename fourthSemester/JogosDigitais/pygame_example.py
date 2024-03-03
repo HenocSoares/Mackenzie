e8208da483db6c665dc8320b8be2570f4c7b0802
Henoc Freire <henocsoares@hotmail.com.br>
2024-03-03 02:23:55 -0300

    Create pygame_example.py

---
+++ fourthSemester/JogosDigitais/pygame_example.py
@@ -0,0 +1,14 @@
+# import da library pygame
+import pygame
+
+# init dos módulos dela
+pygame.init( )
+
+# set de window_size
+screen = pygame.display.set_mode((600, 400))
+
+# screen color: white
+screen.fill((255, 255, 255))
+
+# screen color update
+pygame.display.flip( )
