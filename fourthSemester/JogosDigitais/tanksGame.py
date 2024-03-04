7dd0ee886ffdaca227fe867e3adbb398581b4b32
Henoc Freire <henocsoares@hotmail.com.br>
2024-03-04 00:40:51 -0300

    Update tanksGame.py

--- fourthSemester/JogosDigitais/tanksGame.py
+++ fourthSemester/JogosDigitais/tanksGame.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # O construtor que define os atributos da nossa classe (name, alive, ammo & armor)
 class Tank (object):
@@ -29,3 +30,25 @@ class Tank (object):
             self.explode( )
     
     def explode(self):
+        self.alive = False
+        print(self.name, "explodes!")
+
+t1 = Tank("Tanque1")
+t2 = Tank("Tanque2")
+t3 = Tank("Tanque3")
+t4 = Tank("Tanque4")
+t5 = Tank("Tanque5")
+
+array = [t2, t2, t3, t4, t5]
+
+while(len(array) != 1):
+    num = random.randint(0, len(array)-1)
+    num2 = random.randint(0, len(array)-1)
+    while num2 == num:
+        num2 = random.randint(0, len(array)-1)
+
+    array[num].fire_at(array[num2])
+    if(array[num2].alive != True):
+        array.remove(array[num2])
+
+print("\nO tanque", array[0].name, "venceu!")
