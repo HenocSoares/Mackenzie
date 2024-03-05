25c69692087e963c1dd68929f7408bba50a34389
Henoc Freire <henocsoares@hotmail.com.br>
2024-03-05 18:08:31 -0300

    Update convites.py

--- firstSemester/beecrowd/convites.py
+++ firstSemester/beecrowd/convites.py
@@ -8,11 +8,18 @@ b) A quantidade de convites que devem ser vendidos para que se tenha um lucro de
 Observe que as quantidades calculadas devem ser um número inteiro, portanto, o resultado da quantidade de convites deve ser arredondada para cima, usando a função matemática apropriada em Python.
 '''
 
+import math
+
 custo = float(input("Entre com o custo total: "))
-preco = float(input("Informe o preço do convite unitário: "))
+convite = float(input("Informe o preço do convite unitário: "))
 
 # A quantidade de vendas (em convites) para pelo menos alcançar o custo do espetáculo.
 
-
+qtd = math.ceil(custo / convite)
 
 # A quantidade de convites que devem ser vendidos para que se tenha um lucro de 23%.
+
+lucro = custo * 1.23
+
+print("Quantidade mínima para alcançar o custo: ", qtd)
+print("O necessário para se obter lucro de 23%: ", lucro)
